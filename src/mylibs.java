55344a1dad97c2cc6b56e14e608cbbd74b67f5c0
markfine <Mark.Fine@ds-iq.com>
2013-02-13 17:58:01 -0800

    Update src/mylibs.java
    A small fix in the printed text.

--- src/mylibs.java
+++ src/mylibs.java
@@ -1,11 +1,10 @@
-
 public class mylibs {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.print("Hello world  ha ha ha !!!!!!!");
+		System.out.print("Hello world  ha ha ha !!!!!!! From GIT HUB!!!");
 
 	}
 
